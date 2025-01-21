0c4891ec502e90c1146cd3e7d938f5ce64fe4790
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2025-01-21 16:24:41 +0700

    feat: add TLS credential loading and certificate generation script

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,10 +1,13 @@
 package utils
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/goodsign/monday"
+	"google.golang.org/grpc/credentials"
 )
 
 func ParseTime(dateTimeStr string) time.Time {
@@ -15,4 +18,19 @@ func ParseTime(dateTimeStr string) time.Time {
 		return time.Time{}
 	}
 	return t
+}
+
+func LoadTLSCredentials() (credentials.TransportCredentials, error) {
+	serverCert, err := tls.LoadX509KeyPair("certs/server-cert.pem", "certs/server-key.pem")
+	if err != nil {
+		log.Fatalln("Failed to read server certificate:", err)
+		return nil, err
+	}
+
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.NoClientCert,
+	}
+
+	return credentials.NewTLS(config), nil
 }
